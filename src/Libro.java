d5f9d0fe7352bcc0c2fe63ad1dbcf2036dc38572
GermanG55 <grmnrtr@gmail.com>
2022-09-09 10:28:36 -0500

    por fin

--- src/Libro.java
+++ src/Libro.java
@@ -1,2 +1,11 @@
 public class Libro {
+    public boolean disponible;
+    public String titulo, autor, categoria;
+
+    public Libro(boolean disponible, String titulo, String autor, String categoria) {
+        this.disponible = disponible;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.categoria = categoria;
+    }
 }
