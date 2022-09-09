c9f48acf30d2d341a84d0ff2f9da4f1f07c206db
Santiago Santos <santiago_sabad@hotmail.com>
2022-09-09 11:42:56 -0500

    Opciones de insertar, buscar la cantidad de libros, buscar por autor.

--- src/Libreria.java
+++ src/Libreria.java
@@ -3,10 +3,35 @@ import java.util.List;
 public class Libreria {
     private List<Libro> libros;
 
-    public void cantidadLibros() {
+    public int cantidadLibros(Libro libro) {
+        int result = 0;
+
+        for(int index = 0; 0 >= libros.size(); index++) {
+            if(libro.getTitulo().equals(libros.get(index).getTitulo())) {
+                result = result + 1;
+            }
+        }
+
+        return result;
     }
 
-    public Libro insert() {
-        return null;
+    public Libro insertar(Libro libro) {
+        if(cantidadLibros(libro) < 5) {
+            libros.add(libro);
+        }
+
+        return libro;
+    }
+
+    public Libro buscarPorAutor(String author) {
+        Libro result = new Libro();
+
+        for(Libro l: libros) {
+            if(author.equals(l.getAutor())) {
+                result = l;
+            }
+        }
+
+        return result;
     }
 }
