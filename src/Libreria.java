a50ae5834ebd571a0b18a98a24f8c7395d8799d6
Santiago Santos <santiago_sabad@hotmail.com>
2022-09-09 10:47:02 -0500

    Clase librería creada

---
+++ src/Libreria.java
@@ -0,0 +1,12 @@
+import java.util.List;
+
+public class Libreria {
+    private List<Libro> libros;
+
+    public void cantidadLibros() {
+    }
+
+    public Libro insert() {
+        return null;
+    }
+}
